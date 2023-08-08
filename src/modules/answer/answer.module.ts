36a18bb26b1f543d00441a890b652e436e622984
VanThen60hz <nguyenvthang2409@gmail.com>
2023-08-08 14:47:35 +0700

    feat: answer be

--- src/modules/answer/answer.module.ts
+++ src/modules/answer/answer.module.ts
@@ -4,10 +4,17 @@ import { AnswerController } from './answer.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Answer } from './entities/answer.entity';
 import { Role } from '@modules/role/entities/role.entity';
+import { RoleModule, UserModule } from '..';
+import { UserService } from '@modules/user/user.service';
+import { User } from '@modules/user/entities/user.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Answer, Role])],
+	imports: [
+		TypeOrmModule.forFeature([Answer, Role, User]),
+		UserModule,
+		RoleModule,
+	],
 	controllers: [AnswerController],
-	providers: [AnswerService],
+	providers: [AnswerService, UserService],
 })
 export class AnswerModule {}
