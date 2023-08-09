df7fb46ea7ef277350cce8af00ee219fb5cecafe
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2023-08-09 08:52:27 +0700

    fix: role question service

--- src/modules/question/question.controller.ts
+++ src/modules/question/question.controller.ts
@@ -6,6 +6,7 @@ import {
 	Patch,
 	Param,
 	Delete,
+	UseGuards,
 } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { CreateQuestionDto } from './dto/create-question.dto';
@@ -14,27 +15,35 @@ import { Roles } from '@common/decorators/Roles.decorator';
 import { ROLE } from '@common/enums';
 import { ResponseItem } from './dto/ResponseItem.dto';
 import { Question } from './entities/question.entity';
+import { AuthGuard, RoleGuard } from '@common/guards';
 
 @Controller('question')
 export class QuestionController {
 	constructor(private readonly questionService: QuestionService) {}
 
 	@Roles(ROLE.TEACHER)
+	@UseGuards(AuthGuard, RoleGuard)
 	@Post()
 	create(@Body() createQuestionDto: CreateQuestionDto) {
 		return this.questionService.create(createQuestionDto);
 	}
 
+	@Roles(ROLE.TEACHER, ROLE.STUDENT)
+	@UseGuards(AuthGuard, RoleGuard)
 	@Get()
 	findAll() {
 		return this.questionService.findAll();
 	}
 
+	@Roles(ROLE.TEACHER, ROLE.STUDENT)
+	@UseGuards(AuthGuard, RoleGuard)
 	@Get(':id')
 	findOne(@Param('id') id: string) {
 		return this.questionService.findOne(id);
 	}
 
+	@Roles(ROLE.TEACHER)
+	@UseGuards(AuthGuard, RoleGuard)
 	@Patch(':id')
 	update(
 		@Param('id') id: string,
@@ -43,6 +52,8 @@ export class QuestionController {
 		return this.questionService.update(id, updateQuestionDto);
 	}
 
+	@Roles(ROLE.TEACHER)
+	@UseGuards(AuthGuard, RoleGuard)
 	@Delete(':id')
 	remove(@Param('id') id: string): Promise<ResponseItem<Question>> {
 		return this.questionService.remove(id);
--- src/modules/question/question.module.ts
+++ src/modules/question/question.module.ts
@@ -4,10 +4,17 @@ import { QuestionController } from './question.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Question } from './entities/question.entity';
 import { Role } from '@modules/role/entities/role.entity';
+import { UserService } from '@modules/user/user.service';
+import { RoleModule, UserModule } from '..';
+import { User } from '@modules/user/entities/user.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Question, Role])],
+	imports: [
+		TypeOrmModule.forFeature([Question, Role, User]),
+		UserModule,
+		RoleModule,
+	],
 	controllers: [QuestionController],
-	providers: [QuestionService],
+	providers: [QuestionService, UserService],
 })
 export class QuestionModule {}
